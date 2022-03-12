8a8835987deb4b6395068034008f582dc3e95bef
Yulia Chaika <close.things@gmail.com>
2022-03-12 18:17:28 +0300

    Исправления после замечаний

--- script.js
+++ script.js
@@ -1,45 +1,29 @@
 let popup = document.getElementById("popupForm");
-let popupContainer = document.querySelector(".popup__container")
-let popupEditbtn = document.querySelector(".profile__edit-button");
-let popupClosebtn = document.querySelector(".popup__close-button");
+let popupContainer = document.querySelector(".container");
+let popupEditBtn = document.querySelector(".profile__edit-button");
+let popupCloseBtn = document.querySelector(".popup__close-button");
 let userName = document.querySelector(".profile__name");
 let userWhois = document.querySelector(".profile__whois");
 let popupUserName = document.getElementById("popup-name");
 let popupUserWhois = document.getElementById("popup-whois");
 
-
-
 function openPopup() {
- popup.classList.add("popup_opened");
-popupUserName.value = userName.textContent;
+  popup.classList.add("popup_opened");
+  popupUserName.value = userName.textContent;
+  popupUserWhois.value = userWhois.textContent;
 }
 
 function closePopup() {
   popup.classList.remove("popup_opened");
- 
- }
-
-function formSubmitHandler (evt) {
-  evt.preventDefault(); // Эта строчка отменяет стандартную отправку формы.
-  userName.textContent = popupUserName.value;                                  // Так мы можем определить свою логику отправки.
-  userWhois.textContent = popupUserWhois.value;                                     // О том, как это делать, расскажем позже.
-
-  // Получите значение полей jobInput и nameInput из свойства value
-
-  // Выберите элементы, куда должны быть вставлены значения полей
-
-  // Вставьте новые значения с помощью textContent
 }
 
+function formSubmitHandler(evt) {
+  evt.preventDefault(); // Эта строчка отменяет стандартную отправку формы.
+  userName.textContent = popupUserName.value; // Так мы можем определить свою логику отправки.
+  userWhois.textContent = popupUserWhois.value; // О том, как это делать, расскажем позже.
+  closePopup();
+}
 
- popupUserName.value = userName.textContent;
- popupUserWhois.value = userWhois.textContent;
-
-
-popupEditbtn.addEventListener("click", openPopup);
-popupClosebtn.addEventListener("click", closePopup);
-popupContainer.addEventListener('submit', formSubmitHandler); 
-
-
-
-
+popupEditBtn.addEventListener("click", openPopup);
+popupCloseBtn.addEventListener("click", closePopup);
+popupContainer.addEventListener("submit", formSubmitHandler);
